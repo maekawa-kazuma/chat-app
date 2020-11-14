ad004f00f9433afc911940cac184f269b14b29a1
maekawa <kazumaekawa7218@gmail.com>
2020-11-14 16:06:59 +0900

    Update messages_spec.rb

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -27,19 +27,12 @@ RSpec.describe 'メッセージ投稿機能', type: :system do
 
       # 画像選択フォームに画像を添付す
       # 送信した値がDBに保存されていることを確認する
-      expect do
-        find('input[name="commit"]').click
-      end.to change { Message.count }.by(1)
       # 投稿一覧画面に遷移していることを確認する
     end
     it 'テキストと画像の投稿に成功すること' do
       # サインインする
 
       # 添付する画像を定義する
-      image_path = Rails.root.join('public/images/test_image.png')
-
-      # 画像選択フォームに画像を添付する
-      attach_file('message[image]', image_path, make_visible: true)
       # 値をテキストフォームに入力する
       # 送信した値がDBに保存されていることを確認する
       # expect  do
