61393f5d2db35bb5c6e60faa7ae90762fc44c523
maekawa <kazumaekawa7218@gmail.com>
2020-11-14 16:03:45 +0900

    Update messages_spec.rb

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -11,10 +11,6 @@ RSpec.describe 'メッセージ投稿機能', type: :system do
   context '投稿に成功したとき' do
     it 'テキストの投稿に成功すると、投稿一覧に遷移して、投稿した内容が表示されている' do
       # サインインする
-      sign_in(@room_user.user)
-
-      # 作成されたチャットルームへ遷移する
-      click_on(@room_user.room.name)
 
       # 値をテキストフォームに入力する
       # 送信した値がDBに保存されていることを確認する
@@ -25,10 +21,6 @@ RSpec.describe 'メッセージ投稿機能', type: :system do
     end
     it '画像の投稿に成功すると、投稿一覧に遷移して、投稿した画像が表示されている' do
       # サインインする
-      sign_in(@room_user.user)
-
-      # 作成されたチャットルームへ遷移する
-      click_on(@room_user.room.name)
 
       # 添付する画像を定義する
       # image_path = Rails.root.join('public/images/test_image.png')
@@ -42,10 +34,6 @@ RSpec.describe 'メッセージ投稿機能', type: :system do
     end
     it 'テキストと画像の投稿に成功すること' do
       # サインインする
-      sign_in(@room_user.user)
-
-      # 作成されたチャットルームへ遷移する
-      click_on(@room_user.room.name)
 
       # 添付する画像を定義する
       image_path = Rails.root.join('public/images/test_image.png')
